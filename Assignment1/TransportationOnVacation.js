f19384d448b44ba8f63ce3f9611dbd35eeb9c1bc
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-25 16:20:13 +0530

    added return statement

--- Assignment1/TransportationOnVacation.js
+++ Assignment1/TransportationOnVacation.js
@@ -1,14 +1,13 @@
-function TotalRentalCarCost(d) {
- 
-  // d is the number of days
-    if (d >= 3 && d < 7)
-    {
-      console.log((d*40)-20);
+function rentalCarCost(d) {
+
+    // d is the number of days
+    if (d >= 3 && d < 7) {
+        return (d * 40) - 20;
     }
-  
-    else if(d>=7){
-      console.log((d*40)-50);
-      }
+
+    else if (d >= 7) {
+        return (d * 40) - 50;
+    }
     else
-      console.log(d*40);
- }
+        return d * 40;
+}
