b53deab96b8c7709ae46e8bc6b1c149da33f7580
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-25 14:44:37 +0530

    Made some code alignment changes

--- Assignment1/TransportationOnVacation.js
+++ Assignment1/TransportationOnVacation.js
@@ -1,15 +1,14 @@
 function TotalRentalCarCost(d) {
  
   // d is the number of days
-  if(d>=3&&d<7){
-  console.log((d*40)-20);
-  }
+    if (d >= 3 && d < 7)
+    {
+      console.log((d*40)-20);
+    }
   
-  else if(d>=7){
-  console.log((d*40)-50);
-  }
-  
-  else
-  console.log(d*40);
-  
-}
+    else if(d>=7){
+      console.log((d*40)-50);
+      }
+    else
+      console.log(d*40);
+ }
