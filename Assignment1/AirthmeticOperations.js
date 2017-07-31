1e9094f8ea4bbfb3ca79e91cf4db286c743902d7
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-31 17:04:28 +0530

    addded indentation to all programs

--- Assignment1/AirthmeticOperations.js
+++ Assignment1/AirthmeticOperations.js
@@ -3,7 +3,7 @@ function basicOp(operation, value1, value2)
   switch(operation)
   {
   
-  case '+': return value1+value2;
+  case '+': return  value1+value2;
              break;
              
   case '-': return value1-value2;
