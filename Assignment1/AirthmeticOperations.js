f1c1d0efd826cb8de6d86d9539453eb989a5c025
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-31 17:05:29 +0530

    added indentation

--- Assignment1/AirthmeticOperations.js
+++ Assignment1/AirthmeticOperations.js
@@ -1,19 +1,17 @@
-function basicOp(operation, value1, value2)
-{
-  switch(operation)
-  {
-  
-  case '+': return  value1+value2;
-             break;
-             
-  case '-': return value1-value2;
-             break;
-             
-  case '*': return value1*value2;
-             break;
-             
-  case '/': return value1/value2;
-             break;
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+
+    case '+': return value1 + value2;
+      break;
+
+    case '-': return value1 - value2;
+      break;
+
+    case '*': return value1 * value2;
+      break;
+
+    case '/': return value1 / value2;
+      break;
   }
-  
+
 }
