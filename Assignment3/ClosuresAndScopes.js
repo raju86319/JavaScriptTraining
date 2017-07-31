1cc5a0cb71494af8e0bcd56a7247fbc368113b30
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-31 17:55:44 +0530

    made changes as per the comments given by mentor

--- Assignment3/ClosuresAndScopes.js
+++ Assignment3/ClosuresAndScopes.js
@@ -2,7 +2,6 @@ function createFunctions(numberOfFunctions) {
   var callbacks = [];
   for (var i = 0; i < numberOfFunctions; i++) {
     callbacks.push(makeCallback(i));
-
   }
   return callbacks;
 }
