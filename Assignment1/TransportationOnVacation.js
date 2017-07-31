b95b7b325f505f8d43e95abdeb935f17b59c4da4
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-07-31 18:18:30 +0530

    made changes as per the comments given by mentor

--- Assignment1/TransportationOnVacation.js
+++ Assignment1/TransportationOnVacation.js
@@ -2,11 +2,9 @@ function rentalCarCost(totalDays) {
     var totalCost;
     if (totalDays >= 3 && totalDays < 7) {
         totalCost = (totalDays * 40) - 20;
-    }
-    else if (totalDays >= 7) {
+    } else if (totalDays >= 7) {
         totalCost = (totalDays * 40) - 50;
-    }
-    else
+    } else
         totalCost = totalDays * 40;
     return totalCost;
 }
