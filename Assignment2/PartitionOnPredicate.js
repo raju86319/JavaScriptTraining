96fbb995a95a60a24b6109c430148ccd514eceba
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-08-02 13:08:54 +0530

    Added ternary operator in PartitionOnPredicate.js

--- Assignment2/PartitionOnPredicate.js
+++ Assignment2/PartitionOnPredicate.js
@@ -2,11 +2,7 @@ function partitionOn(predicate, items) {
     var filterTrueitems = [];
     var filterFalseitems = [];
     items.forEach(function (value) {
-        if (predicate(value)) {
-            filterTrueitems.push(value);
-        } else {
-            filterFalseitems.push(value);
-        }
+        predicate(value) ? filterTrueitems.push(value) : filterFalseitems.push(value);
     });
     items.length = 0;
     items.push.apply(items, filterFalseitems.concat(filterTrueitems));
