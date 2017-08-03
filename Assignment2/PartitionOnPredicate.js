0ef27de678fcc0818064bb680a24678d574f9ba9
neelam Rajendra naidu <neelam_naidu@epam.com>
2017-08-03 12:09:49 +0530

    changes done as per mentor's comments

--- Assignment2/PartitionOnPredicate.js
+++ Assignment2/PartitionOnPredicate.js
@@ -2,7 +2,7 @@ function partitionOn(predicate, items) {
     var filterTrueitems = [];
     var filterFalseitems = [];
     items.forEach(function (value) {
-        predicate(value) ? filterTrueitems.push(value) : filterFalseitems.push(value);
+         predicate(value) ? filterTrueitems.push(value) : filterFalseitems.push(value);
     });
     items.length = 0;
     items.push.apply(items, filterFalseitems.concat(filterTrueitems));
